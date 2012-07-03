814f17cbfb1f588deddf31766b460b588a7a757f
Marush Denchev <avreon@gmail.com>
2012-07-03 12:56:30 +0300

    Adding bulgarian translation

---
+++ localization/jquery-ui-timepicker-bg.js
@@ -0,0 +1,20 @@
+/* Bulgarian translation for the jQuery Timepicker Addon */
+/* Written by Marush Denchev */
+(function($) {
+	$.timepicker.regional['bg'] = {
+		timeOnlyTitle: 'Изберете време',
+		timeText: 'Време',
+		hourText: 'Час',
+		minuteText: 'Минути',
+		secondText: 'Секунди',
+		millisecText: 'Милисекунди',
+		timezoneText: 'Часова зона',
+		currentText: 'В момента',
+		closeText: 'Затвори',
+		timeFormat: 'hh:mm tt',
+		amNames: ['AM', 'A'],
+		pmNames: ['PM', 'P'],
+		ampm: false
+	};
+	$.timepicker.setDefaults($.timepicker.regional['bg']);
+})(jQuery);
